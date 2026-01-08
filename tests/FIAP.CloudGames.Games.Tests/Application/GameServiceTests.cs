b04c83c1abc740559f98999723062c115f8ab716
Matheus Soares Camacho <matheus_camacho27@hotmail.com>
2026-01-08 20:25:43 -0300

    GameTest Correction

---
+++ tests/FIAP.CloudGames.Games.Tests/Application/GameServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using FIAP.CloudGames.Games.Application.Dtos;
+using FIAP.CloudGames.Games.Application.Interfaces;
+using FIAP.CloudGames.Games.Application.Services;
+using FIAP.CloudGames.Games.Domain.Entities;
+using FIAP.CloudGames.Games.Domain.Interfaces.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP.CloudGames.Games.Tests.Application
+{
+    public class GameServiceTests
+    {
+        [Fact]
+        public async Task CreateAsync_Should_Save_Game_And_Index_In_Elasticsearch()
+        {
+            // Arrange
+            var repositoryMock = new Mock<IGameRepository>();
+            var searchMock = new Mock<IGameSearchService>();
+
+            repositoryMock
+                .Setup(r => r.AddAsync(It.IsAny<Game>()))
+                .Returns(Task.CompletedTask);
+
+            searchMock
+                .Setup(s => s.IndexAsync(It.IsAny<Game>(), default))
+                .Returns(Task.CompletedTask);
+
+            var service = new GameService(
+                repositoryMock.Object,
+                searchMock.Object
+            );
+
+            var dto = new CreateGameDto
+            {
+                Title = "God of War",
+                Price = 349.90m,
+                Description = "Action Adventure",
+                ReleaseDate = new DateTime(2022, 11, 9),
+                Developer = "Santa Monica Studio",
+                Publisher = "Sony"
+            };
+
+            // Act
+            var game = await service.CreateAsync(dto);
+
+            // Assert
+            repositoryMock.Verify(r => r.AddAsync(It.IsAny<Game>()), Times.Once);
+            searchMock.Verify(s => s.IndexAsync(It.IsAny<Game>(), default), Times.Once);
+
+            Assert.NotNull(game);
+            Assert.Equal(dto.Title, game.Title);
+        }
+    }
+}
---
+++ tests/FIAP.CloudGames.Games.Tests/Domain/GameTests.cs
@@ -0,0 +1,43 @@
+﻿using FIAP.CloudGames.Games.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP.CloudGames.Games.Tests.Domain
+{
+    public class GameTests
+    {
+        [Fact]
+        public void Create_Game_Should_Generate_Id_And_Set_Properties()
+        {
+            // Arrange
+            var title = "Elden Ring";
+            var price = 299.90m;
+            var description = "Soulslike RPG";
+            var releaseDate = new DateTime(2022, 02, 25);
+            var developer = "FromSoftware";
+            var publisher = "Bandai Namco";
+
+            // Act
+            var game = new Game(
+                title,
+                price,
+                description,
+                releaseDate,
+                developer,
+                publisher
+            );
+
+            // Assert
+            Assert.NotEqual(Guid.Empty, game.Id);
+            Assert.Equal(title, game.Title);
+            Assert.Equal(price, game.Price);
+            Assert.Equal(description, game.Description);
+            Assert.Equal(releaseDate, game.ReleaseDate);
+            Assert.Equal(developer, game.Developer);
+            Assert.Equal(publisher, game.Publisher);
+        }
+    }
+}
